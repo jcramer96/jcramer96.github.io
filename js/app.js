8670b42b34d6af984547be2f175a0ad9524dd7d9
Jon Cramer <joncramer96@gmail.com>
2023-05-25 22:56:45 -0400

    toggle buttons for project info

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
+/* ============================================= */
+/*          NAVBAR TOGGLE BUTTON                 */
+/* ============================================= */
 const toggleBtn = document.getElementsByClassName("toggle-btn")[0];
 const navbarLinks = document.getElementsByClassName("navbar-links")[0];
 
@@ -5,3 +8,16 @@ toggleBtn.addEventListener('click', (e) => {
     e.preventDefault()
     navbarLinks.classList.toggle('active');
 })
+
+/* ============================================= */
+/*          PROJECT INFO TOGGLE BUTTON           */
+/* ============================================= */
+let projectInfoToggle = document.querySelectorAll(".project-toggle-btn");
+let projectDetails = document.querySelectorAll(".project-details");
+
+for (let i = 0; i < projectInfoToggle.length; i++) {
+    projectInfoToggle[i].addEventListener("click", () => {
+        projectInfoToggle[i].style.transform = 'rotate(180deg)';
+        projectDetails[i].classList.toggle('active');
+    })
+}
