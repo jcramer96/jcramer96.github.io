417d833ee322346624d17e11c8b89a61646a14ea
Jon Cramer <joncramer96@gmail.com>
2023-05-27 18:07:31 -0400

    contact form submit feature

--- js/app.js
+++ js/app.js
@@ -21,3 +21,28 @@ for (let i = 0; i < projectInfoToggle.length; i++) {
         projectInfoToggle[i].classList.toggle('active');
     })
 }
+
+/* ============================================= */
+/*              SUBMIT FORM BUTTON               */
+/* ============================================= */
+const submitBtn = document.getElementById("submit-btn");
+const userName = document.getElementById("name");
+const userEmail = document.getElementById("email");
+const userMessage = document.getElementById("message");
+
+submitBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (userName.value == "" || userEmail.value == "" || userMessage.value == "") {
+        alert ("Please make sure to fill out all fields before submitting.");
+    } else {
+        Email.send({
+            SecureToken : "86712dc0-ab8b-4707-a9b9-160f1582aa01",
+            To : 'joncramer96@gmail.com',
+            From : userEmail.value,
+            Subject : "New Contact Form Inquiry",
+            Body : userMessage.value
+        }).then(
+          message => alert(message)
+        )
+    }
+});
